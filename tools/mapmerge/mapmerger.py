1843035df21373d548ceb851e20bb16690c652b6
CitadelStationBot <CitadelStationBot>
2017-05-29 16:16:24 -0500

    Fixes mapmerge

--- tools/mapmerge/mapmerger.py
+++ tools/mapmerge/mapmerger.py
@@ -52,8 +52,8 @@ def main(map_folder, tgm=0):
         print("\nAborted map merge.")
         sys.exit()
     else:
-        for i in valid_indices:
-            path_str = str(list_of_files[i])
+        for i in maps.indices:
+            path_str = str(maps.files[i])
             shutil.copyfile(path_str, path_str + ".before")
             path_str_pretty = path_str[len(map_folder):]
             try:
