3df765f5c78181213bcd96ca86b5372eed1dc31e
Letter N <24603524+LetterN@users.noreply.github.com>
2020-07-21 14:08:26 +0800

    things and things. paperwork gone??

--- tgui/packages/tgui/components/Input.js
+++ tgui/packages/tgui/components/Input.js
@@ -83,10 +83,6 @@ export class Input extends Component {
     if (input) {
       input.value = toInputValue(nextValue);
     }
-
-    if (this.props.autoFocus) {
-      setTimeout(() => input.focus(), 1);
-    }
   }
 
   componentDidUpdate(prevProps, prevState) {
