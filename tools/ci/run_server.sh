ce71d99fc8b24b92b5d7f91a0b27bd525772e15c
Putnam3145 <putnam3145@gmail.com>
2021-07-25 15:36:37 -0700

    why are there so many files

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -7,10 +7,10 @@ mkdir ci_test/config
 #test config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
-#throw extools into ldd
-cp libbyond-extools.so ~/.byond/bin/libbyond-extools.so
-chmod +x ~/.byond/bin/libbyond-extools.so
-ldd ~/.byond/bin/libbyond-extools.so
+#throw auxmos into ldd
+cp libauxmos.so ~/.byond/bin/libauxmos.so
+chmod +x ~/.byond/bin/libauxmos.so
+ldd ~/.byond/bin/libauxmos.so
 
 cd ci_test
 DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
