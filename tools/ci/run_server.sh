b7af1770125980972e567016fe032f15e868045a
Putnam3145 <putnam3145@gmail.com>
2021-07-25 16:54:15 -0700

    Maybe I Can Just Remove Them

--- tools/ci/run_server.sh
+++ tools/ci/run_server.sh
@@ -7,11 +7,6 @@ mkdir ci_test/config
 #test config
 cp tools/ci/ci_config.txt ci_test/config/config.txt
 
-#throw auxmos into ldd
-cp libauxmos.so ~/.byond/bin/libauxmos.so
-chmod +x ~/.byond/bin/libauxmos.so
-ldd ~/.byond/bin/libauxmos.so
-
 cd ci_test
 DreamDaemon tgstation.dmb -close -trusted -verbose -params "log-directory=ci"
 cd ..
