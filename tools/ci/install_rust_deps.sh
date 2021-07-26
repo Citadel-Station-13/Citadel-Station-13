c78a7af84872bc661a9fcc7e0ce6c051fb6595ac
Putnam3145 <putnam3145@gmail.com>
2021-07-25 17:01:36 -0700

    byond version, also more like this

--- tools/ci/install_rust_deps.sh
+++ tools/ci/install_rust_deps.sh
@@ -5,8 +5,6 @@ source dependencies.sh
 
 mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
-wget -O ~/.byond/bin/libauxmos.so "https://github.com/Putnam3145/auxmos/releases/download/$AUXMOS_VERSION/libauxmos.so"
+wget -O libauxmos.so "https://github.com/Putnam3145/auxmos/releases/download/$AUXMOS_VERSION/libauxmos.so"
 chmod +x ~/.byond/bin/librust_g.so
-chmod +x ~/.byond/bin/libauxmos.so
 ldd ~/.byond/bin/librust_g.so
-ldd ~/.byond/bin/libauxmos.so
