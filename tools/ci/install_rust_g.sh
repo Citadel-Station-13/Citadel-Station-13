94871ab34e0f2797e52f02f73a98b5def54e5e78
Putnam3145 <putnam3145@gmail.com>
2021-07-25 17:55:12 -0700

    just copy from bee (4head)

--- tools/ci/install_rust_g.sh
+++ tools/ci/install_rust_g.sh
@@ -5,6 +5,5 @@ source dependencies.sh
 
 mkdir -p ~/.byond/bin
 wget -O ~/.byond/bin/librust_g.so "https://github.com/tgstation/rust-g/releases/download/$RUST_G_VERSION/librust_g.so"
-wget -O libauxmos.so "https://github.com/Putnam3145/auxmos/releases/download/$AUXMOS_VERSION/libauxmos.so"
 chmod +x ~/.byond/bin/librust_g.so
 ldd ~/.byond/bin/librust_g.so
