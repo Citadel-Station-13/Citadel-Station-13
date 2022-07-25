84cd0054353ea3866834774c982eb1e5e25e2524
silicons <2003111+silicons@users.noreply.github.com>
2022-07-24 23:28:51 -0700

    Update storage.js

--- tgui/packages/common/storage.js
+++ tgui/packages/common/storage.js
@@ -12,7 +12,7 @@ export const IMPL_INDEXED_DB = 2;
 
 const INDEXED_DB_VERSION = 2;
 const INDEXED_DB_NAME = 'tgui-citadel-main';
-const INDEXED_DB_STORE_NAME = 'tgui-citadel-main-v1';
+const INDEXED_DB_STORE_NAME = 'tgui-store-citadel-main';
 
 const READ_ONLY = 'readonly';
 const READ_WRITE = 'readwrite';
